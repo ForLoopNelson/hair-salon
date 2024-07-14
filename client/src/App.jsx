0f589dc0755f1c93b70167d902d7234f88c50aea
Cory Nelson <fullnelsontech@gmail.com>
2024-07-14 16:58:26 -0400

    completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import { Outlet } from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
+//Create a global header for all pages on the site, and render the target of the clicked route as the body of the page.
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <header>
+        <h1 id="title">Holland Hair Salon</h1>
+        <nav>
+          <a href="/">Home</a> | <a href="/staff">Staff</a> |{" "}
+          <a href="/services">Services</a> | <a href="/booking">Booking</a> |{" "}
+          <a href="/admin">Admin</a>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
   )
 }
 
