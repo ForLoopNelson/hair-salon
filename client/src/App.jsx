c3e1049ca510ee6c1cd3d52de753943f513da7cd
Cory Nelson <fullnelsontech@gmail.com>
2024-07-15 12:53:46 -0400

    add className and style tag in header for salon Image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react"
 import { Outlet } from "react-router-dom"
+import background from "../public/salon1.jpg"
 
 //Create a global header for all pages on the site, and render the target of the clicked route as the body of the page.
 
 const App = () => {
   return (
     <div>
-      <header>
+      <header
+        className="img-fit"
+        style={{
+          backgroundImage: `url(${background})`,
+        }}
+      >
         <h1 id="title">Holland Hair Salon</h1>
         <nav>
           <a href="/">Home</a> | <a href="/staff">Staff</a> |{" "}
