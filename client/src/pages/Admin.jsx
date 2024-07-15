e3953138b3c58d669a6887dc95d02ad8a8d51e83
Cory Nelson <fullnelsontech@gmail.com>
2024-07-15 12:55:29 -0400

    final upload, except for maybe styling changes

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,7 +1,32 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 const Admin = () => {
-  return <h1>This is Admin</h1>
+  const [bookings, setBookings] = useState([])
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+      const response = await fetch("http://localhost:5000/api/bookings")
+      const data = await response.json()
+      setBookings(data)
+    }
+
+    fetchBookings()
+  }, [])
+
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <ul>
+        {bookings.map((booking) => (
+          <li key={booking._id}>
+            {booking.name} - {booking.email} -{" "}
+            {new Date(booking.date).toLocaleDateString()} - {booking.service} -{" "}
+            {booking.stylist}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Admin
