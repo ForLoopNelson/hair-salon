4f769eeb352c5af4d003b20239e9f3b4a760ba1b
Cory Nelson <fullnelsontech@gmail.com>
2024-07-20 11:25:31 -0400

    made footer file of contact info

---
+++ client/src/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer id="contact">
+      <p className="pad">
+        <span>Visit Us</span> <br />
+        Glamour Hair Salon is conveniently located in the heart of downtown:
+        <br />
+        123 Main Street, Suite 456
+        <br />
+        Your City, State, ZIP Code
+      </p>
+
+      <p className="pad">
+        Phone: (123) 456-7890
+        <br />
+        Email: info@glamourhairsalon.com
+      </p>
+      <p className="pad">
+        Follow us on social media for the latest updates and special offers:
+        <br />
+        <ul>
+          <li>Facebook: @GlamourHairSalon</li>
+          <li>Instagram: @GlamourHairSalon</li>
+          <li>Twitter: @GlamourHair</li>
+        </ul>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
