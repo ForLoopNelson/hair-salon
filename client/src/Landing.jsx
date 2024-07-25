67483f094debab30c037dbe10d9b421d92373020
Cory Nelson <fullnelsontech@gmail.com>
2024-07-25 11:44:09 -0400

    small change still not in use

--- client/src/Landing.jsx
+++ client/src/Landing.jsx
@@ -1,36 +1,15 @@
 import React from "react"
-import { Outlet } from "react-router-dom"
 
 const Landing = () => {
   return (
     <div className="landing">
       <div className="landing-nav">
         <h1>Great Lakes Salon</h1>
-        <nav>
-          <a href="/home">
-            <span>Home</span>
-          </a>{" "}
-          |{" "}
-          <a href="/staff">
-            <span>Staff</span>
-          </a>{" "}
-          |{" "}
-          <a href="/services">
-            <span>Services</span>
-          </a>{" "}
-          |{" "}
-          <a href="/booking">
-            <span>Booking</span>
-          </a>{" "}
-          |{" "}
-          <a href="/admin">
-            <span>Admin</span>
-          </a>
-        </nav>
+        <div className="btn-class">
+          <a href="/">Enter</a>
+        </div>
       </div>
-      <main>
-        <Outlet />
-      </main>
+      <main></main>
     </div>
   )
 }
