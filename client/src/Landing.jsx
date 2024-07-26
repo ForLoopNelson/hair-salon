68932a94725f79f12c7345aca5777ceb309aeac8
Cory Nelson <fullnelsontech@gmail.com>
2024-07-26 12:09:09 -0400

    add custom button and adjust h1 title

--- client/src/Landing.jsx
+++ client/src/Landing.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Landing = () => {
   return (
@@ -6,7 +7,10 @@ const Landing = () => {
       <div className="landing-nav">
         <h1>Great Lakes Salon</h1>
         <div className="btn-class">
-          <a href="/">Enter</a>
+          <Link to="/app/home">
+            <span className="btn-text-one">Welcome!</span>
+            <span className="btn-text-two">Enter</span>
+          </Link>
         </div>
       </div>
       <main></main>
